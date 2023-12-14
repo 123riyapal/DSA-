d53453783ada8097cdc5d0ae99102adcc04de79f
Riya Pal <riya00singh99@gamil.com>
2023-12-14 10:09:28 +0530

    Bubble sort

---
+++ sorting/bubbleSort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+    int didSwap=0;
+        for (int j = 1; j < n-i; j++)
+        {
+            if (arr[j-1] > arr[j])
+            {
+           swap(arr[j-1], arr[j]);
+           didSwap=1;
+            }
+        }
+        if(didSwap==0)
+        break;
+
+        cout<<"it runs"<<endl;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    bubbleSort(arr, n); /* "<-- this take Approx(o(n^2)) time complexity \--> " */
+
+    //  for(int i=0;i<n-1;i++){
+    //     for(int j=i+1;j<n;j++){
+    //         if(arr[i]>arr[j]){   <-- this take o(n^2) time complexity /-->
+    //             swap(arr[i],arr[j]);
+    //         }
+    //     }
+    //  }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
