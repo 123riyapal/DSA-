70042b38b54ab588a460f7ccfa24863c11e7aad1
Riya Pal <riya00singh99@gamil.com>
2024-02-19 21:25:01 +0530

    queue files

--- Queue/circularQueue.cpp
+++ Queue/circularQueue.cpp
@@ -1,67 +1,76 @@
 #include<iostream>
 using namespace std;
-class MyQueue{
-    public:
+
+class MyQueue {
+public:
     int *arr;
     int front;
     int rear;
     int size;
-    MyQueue(int size){
-        arr=new int[size];
-        this->size=size;
-        front=0;
-        rear=0;
+
+    MyQueue(int size) {
+        arr = new int[size];
+        this->size = size;
+        front = 0;
+        rear = 0;
     }
-    void push(int x,int i){
-        if((rear+1)%size!=front){
-            arr[(rear+1)%size]=x;
-            cout<<x<<" pushed index pos"<<i<<"\n";
+
+    void push(int x) {
+        if (isFull()) {
+            cout << "queue overflow" << endl;
+        } else {
+            arr[rear] = x;
+            rear = (rear + 1) % size;
+            cout << x << " pushed into the Queue" << "\n";
         }
     }
-    void pop(){
-        if(front!=rear){
-            front=(front+1)%size;
+
+    void pop() {
+        if (!isEmpty()) {
+            front = (front + 1) % size;
+        } else {
+            cout << "Queue Underflow" << endl;
         }
     }
-    int Top(){
-        if(front==rear)return -1;
-        int top=arr[front];
+
+    int Top() {
+        if (isEmpty()) return -1;
+        int top = arr[front];
         return top;
     }
-    bool isFull(){
-        if(rear==front-1)return true;
-        return false;
-    }
-    bool isEmpty(){
-        if(front==rear)return true;
+
+    bool isFull() {
+        if ((rear + 1) % size == front) return true;
         return false;
     }
 
+    bool isEmpty() {
+        if (front == rear) return true;
+        return false;
+    }
 };
-int main(){
-MyQueue queue(10);
-int i=0;
-queue.push(1,i++);
-queue.push(2,i++);
-queue.push(3,i++);
-queue.push(4,i++);
-queue.push(5,i++);
-queue.push(6,i++);
-queue.push(7,i++);
-cout<<queue.Top()<<endl;
-queue.pop();
-queue.push(9,i++);
-queue.push(10,i++);
-queue.push(8,i++);
 
-cout<<queue.Top()<<endl;
+int main() {
+    MyQueue queue(10);
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+    queue.push(4);
+    queue.push(5);
+    queue.push(6);
+    queue.push(7);
+    cout << queue.Top() << endl;
+    queue.pop();
+    queue.push(9);
+    queue.push(1);
+    queue.push(8);
+    queue.pop();
+    queue.push(8);
 
+    cout << queue.Top() << endl;
 
-cout<<queue.isFull()<<endl;
-cout<<queue.isEmpty()<<endl;
+    cout << "Is Full: " << queue.isFull() << endl;
+    cout << "Is Empty: " << queue.isEmpty() << endl;
 
-
-
-
-return 0;
+    return 0;
 }
--- Queue/implementation.cpp
+++ Queue/implementation.cpp
@@ -12,9 +12,10 @@ class MyQueue{
         front=0;
         rear=0;
     }
-    void push(int x){
+    void push(int x,int i){
         if(rear!=size){
             arr[rear++]=x;
+            cout<< x <<"pushed index pos "<<i<<endl;
         }
     }
     void pop(){
@@ -38,16 +39,27 @@ class MyQueue{
 
 };
 int main(){
+int i=0;
 MyQueue queue(10);
-queue.push(1);
+
+queue.push(1,i++);
+queue.push(2,i++);
+queue.push(3,i++);
+queue.push(4,i++);
+queue.push(5,i++);
+queue.push(6,i++);
+queue.push(7,i++);
 cout<<queue.Top()<<endl;
 queue.pop();
+queue.push(9,i++);
+queue.push(10,i++);
+queue.push(8,i++);
+
 cout<<queue.Top()<<endl;
 
 
 cout<<queue.isFull()<<endl;
 cout<<queue.isEmpty()<<endl;
-
 
 
 
